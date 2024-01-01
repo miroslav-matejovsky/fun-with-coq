b3db24e52349b6face707da376a9def21352a661
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2024-01-01 12:40:50 +0100

    coq in a hurry: defining constants

--- coq_in_hurry/_1_2_Evaluating_expressions.v
+++ coq_in_hurry/_1_2_Evaluating_expressions.v
@@ -1,4 +1,3 @@
 Compute let f := fun x => (x * 3, 3) in f 4.
 
 Compute let f := fun a b c d e => a + b + c + d + e in f 1 2 3 4 5.
-
---
+++ coq_in_hurry/_2_1_defining_new_constants.v
@@ -0,0 +1,16 @@
+Definition example1 := fun x : nat => x*x + 2*x + 1.
+
+Definition a := example1 3.
+
+Compute a.
+
+Print a.
+
+Check example1.
+
+Compute (example1 5).
+
+
+
+
+
